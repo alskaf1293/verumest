73e908d199df3d385887ad13a97ce927ef537e51
alskaf1293 <48730163+alskaf1293@users.noreply.github.com>
2023-06-03 20:34:33 -0700

    added profile picture, but it doesn't work

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -11,21 +11,75 @@ export default function Settings({ user }: { user: User }) {
   const [location, setLocation] = useState('');
   const [name, setName] = useState('');
   const [loading, setLoading] = useState(true);
+  const [profilePictureUrl, setProfilePictureUrl] = useState('');
 
   useEffect(() => {
     fetchProfile();
   }, []);
 
+  const uploadProfilePicture = async (event: any) => {
+    const file = event.target.files[0];
+    const filePath = `${user.id}/profile_picture.${file.name.split('.').pop()}`;
+
+    try {
+        const { error: uploadError } = await supabase.storage.from('profile-pictures').upload(filePath, file);
+
+        if (uploadError) {
+            throw uploadError;
+        }
+
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log('Error uploading: ', error.message);
+        } else {
+            console.log('An unknown error occurred while getting picture URL');
+        }
+        return;
+    }
+    
+    let publicURL = '';
+
+    try {
+        const { data} = await supabase.storage.from('profile-pictures').getPublicUrl(filePath);
+
+        publicURL = data.publicUrl;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log('Error getting picture URL: ', error.message);
+        } else {
+            console.log('An unknown error occurred while getting picture URL');
+        }
+        return;
+    }
+
+    // Update the user profile with the URL of the uploaded picture
+    try {
+        const { error } = await supabase.from('user_profiles').update({ profile_picture_url: publicURL }).eq('user_id', user.id);
+        if (error) {
+            throw error;
+        }
+        // Update the state with the new picture URL
+        setProfilePictureUrl(publicURL);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log('Error updating profile: ', error.message);
+        } else {
+            console.log('An unknown error occurred while updating profile');
+        }
+    }
+  }
+
   const fetchProfile = async () => {
     const { data, error } = await supabase
       .from('user_profiles')
-      .select(`location, name`)
+      .select(`location, name, profile_picture_url`)
       .eq('user_id', user.id)
       .single();
-
+  
     if (data) {
       setLocation(data.location);
       setName(data.name || '');
+      setProfilePictureUrl(data.profile_picture_url || '');
     }
     setLoading(false);
   }
@@ -60,6 +114,8 @@ export default function Settings({ user }: { user: User }) {
                 style={{color: "black"}}
               />
             </label>
+            <img src={profilePictureUrl} alt="Profile Picture" />
+            <input type="file" accept="image/*" onChange={uploadProfilePicture} />
             <button onClick={updateProfile}>Update Profile</button>
           </div>
         )}
